10ace0e5aca7ec0a5d098d18b9ffdb9f2cbd7511
Dino Tudor <dinotudor@gmail.com>
2020-02-19 00:01:21 -0300

    scripts dev and dev:debug, config debug

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hola mundo! Que tal?' });
+  return res.json({ message: `Hola mundo! Que tal? 2+2=?? -> ${2 + 2} ` });
 });
 
 export default routes;
