4b15b3e8eb6d78bcd59a845b62cdc3823f020e1e
Dino Tudor <dinotudor@gmail.com>
2020-02-24 17:54:26 -0300

    migration rollback, user/ recipients migration

--- src/database/migrations/20200224195052-create-recipients.js
+++ src/database/migrations/20200224195052-create-recipients.js
@@ -11,7 +11,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      adress: {
+      address: {
         type: Sequelize.STRING,
         allowNull: false,
       },
