64e473f90cc912673efdd4087724a2b2c9f194cd
Dino Tudor <dinotudor@gmail.com>
2020-02-24 20:10:57 -0300

    password hash for admin edit

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,20 +1,20 @@
 import Admin from '../models/Admin';
 
 class AdminController {
-  async store(req, res) {
-    const userExists = await Admin.findOne({
+  async update(req, res) {
+    /*     const userExists = await Admin.findOne({
       where: { email: req.body.email },
     });
 
     if (userExists) {
       return res.status(400).json({ error: 'Email already used' });
     }
-
-    const { id, name, email } = await Admin.create(req.body);
+ */
+    const { name, email, password_hash } = await Admin.create(req.body);
     return res.json({
-      id,
       name,
       email,
+      password_hash,
     });
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
 import RecipientsController from './app/controllers/RecipientsController';
+import AdminController from './app/controllers/AdminController';
 
 const routes = new Router();
 
+/* ADMIN */
+routes.put('/admin', AdminController.update);
+
+/* RECIPIENTS */
 routes.post('/recipients', RecipientsController.store);
 
+/* TEST */
 routes.get('/', (req, res) => {
   return res.json({ message: `Hola mundo! Que tal? 2+2=?? -> ${2 + 2} ` });
 });
