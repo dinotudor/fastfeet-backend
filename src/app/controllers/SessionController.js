880528e647421b7964e2cf8527b296323913eea4
Dino Tudor <dinotudor@gmail.com>
2020-02-26 12:09:47 -0300

    token return for create session

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,32 +1,35 @@
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+
+import auth from '../../config/auth';
 
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
-    console.log(req.body);
+    // console.log(req.body);
     // console.log(`EMAIL -> ${email} PASSWORD -> ${password}`);
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(401).json({ error: 'Not a user' });
+      return res.status(401).json({ error: 'Not a valid user' });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'password do not match' });
+      return res.status(401).json({ error: 'password does not match' });
     }
 
-    const { name } = user;
+    const { id, name } = user;
 
     return res.json({
       user: {
+        id,
         name,
         email,
-      } /* ,
-      token: jwt.sign({ name }, '0f0bc08b4ee6413f434f83a59afb49d6', {
-        expiresIn: '7d',
-      }), */,
+      },
+      token: jwt.sign({ id }, auth.secret, {
+        expiresIn: auth.expiresIn,
+      }),
     });
   }
 }
