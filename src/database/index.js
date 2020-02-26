d92bcbc59f6223ad083cae5970672c5390feb04d
Dino Tudor <dinotudor@gmail.com>
2020-02-26 11:09:37 -0300

    Session Controller working

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,9 @@
 import Sequelize from 'sequelize';
 
+import databaseConfig from '../config/database';
+
 import Recipients from '../app/models/Recipients';
 import User from '../app/models/User';
-
-import databaseConfig from '../config/database';
 
 const models = [Recipients, User];
 
@@ -14,7 +14,6 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-
     models.map(model => model.init(this.connection));
   }
 }
