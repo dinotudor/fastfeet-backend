684a3546adae51cf46893f25ee2cbdbf18cf86a7
Dino Tudor <dinotudor@gmail.com>
2020-03-11 23:20:26 -0300

    WIP routes import FileController

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from './config/multer';
 
 import RecipientsController from './app/controllers/RecipientsController';
 import SessionController from './app/controllers/SessionController';
+import CourierController from './app/controllers/CouriersController';
 
 import AuthMiddleware from './app/middlewares/auth';
 
@@ -23,8 +24,6 @@ routes.put('/recipients/:id', RecipientsController.update);
 routes.delete('/recipients/:id', RecipientsController.delete);
 
 /* FILES */
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), CourierController.store);
 
 export default routes;
